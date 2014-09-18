4305216541a523ea18d4bbece1c6d26a6b1e91a3
Harrison Ting <runhappy@gmail.com>
2014-09-18 02:46:08 -0700

    Include SFML

--- Source/Asteroids/Asteroids/Main.cpp
+++ Source/Asteroids/Asteroids/Main.cpp
@@ -1,4 +1,11 @@
+// Ignore SFML warnings.
+#pragma warning(push, 0)
+// Setting the warning level to 1 doesn't disable C4350 for some reason, so explicitly disable it.
+#pragma warning(disable : 4350)
+#include <SFML/Graphics.hpp>
+#pragma warning(pop)
+
 int main(unsigned int /*argc*/, const char* /*argv*/[])
 {
-	return 0;
+    return 0;
 }
