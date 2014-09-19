ee7cace91cf13ab65eca323938ca60502c000193
Harrison Ting <runhappy@gmail.com>
2014-09-19 02:02:14 -0700

    Create precomiled header and use it

---
+++ Source/Asteroids/Asteroids/AsteroidsPCH.h
@@ -0,0 +1,6 @@
+// Ignore SFML warnings.
+#pragma warning(push, 0)
+// Setting the warning level to 1 doesn't disable C4350 for some reason, so explicitly disable it.
+#pragma warning(disable : 4350)
+#include <SFML/Graphics.hpp>
+#pragma warning(pop)
--- Source/Asteroids/Asteroids/Main.cpp
+++ Source/Asteroids/Asteroids/Main.cpp
@@ -1,9 +1,4 @@
-// Ignore SFML warnings.
-#pragma warning(push, 0)
-// Setting the warning level to 1 doesn't disable C4350 for some reason, so explicitly disable it.
-#pragma warning(disable : 4350)
-#include <SFML/Graphics.hpp>
-#pragma warning(pop)
+#include "AsteroidsPCH.h"
 
 int main(unsigned int /*argc*/, const char* /*argv*/[])
 {
