8f2e8784e32dd22303843d149115eef23873495a
Harrison Ting <runhappy@gmail.com>
2014-09-27 12:01:56 -0700

    Change the main loop to use be fixed update interval, variable render interval.

--- Source/Asteroids/Asteroids/Main.cpp
+++ Source/Asteroids/Asteroids/Main.cpp
@@ -43,23 +43,43 @@ void Render(sf::RenderWindow& window, GameState& state)
 
 int main(unsigned int /*argc*/, const char* /*argv*/[])
 {
-    const unsigned int kNumParticles = 10000;
+    const unsigned int kNumParticles = 5000;
     GameState state{ kNumParticles };
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "Asteroids");
+    window.setVerticalSyncEnabled(true);
 
-    auto previousUpdateTime = std::chrono::high_resolution_clock::now();
+    auto previousTime = std::chrono::high_resolution_clock::now();
+    std::chrono::microseconds remainingUpdateTime;
 
     while (window.isOpen())
     {
+        const auto currentTime = std::chrono::high_resolution_clock::now();
+        auto elapsedTime(std::chrono::duration_cast<std::chrono::microseconds>(currentTime - previousTime));
+        previousTime = currentTime;
+        remainingUpdateTime += elapsedTime;
+
         ProcessWindowEvents(window, state);
 
-        auto updateTime = std::chrono::high_resolution_clock::now();
-        auto timeDelta = updateTime - previousUpdateTime;
-        auto elapsedTime(std::chrono::duration_cast<std::chrono::microseconds>(timeDelta));
-        previousUpdateTime = updateTime;
+        const unsigned int kDesiredUdatesPerSecond = 60;
+        const auto kUpdateStepDuration = std::chrono::microseconds(1000000) / kDesiredUdatesPerSecond;
+        unsigned int updateIterations = 0;
+        while (remainingUpdateTime >= kUpdateStepDuration)
+        {
+            Update(state, kUpdateStepDuration);
+            remainingUpdateTime -= kUpdateStepDuration;
 
-        Update(state, elapsedTime);
+            // Ensure that we don't spiral out of control
+            ++updateIterations;
+            if (updateIterations > kDesiredUdatesPerSecond)
+            {
+                std::cerr << "Updates are "
+                    << std::chrono::duration_cast<std::chrono::milliseconds>(remainingUpdateTime).count()
+                    << "ms behind" << std::endl;
+                break;
+            }
+        }
+
         Render(window, state);
 
         std::cout << kNumParticles << " particles"
